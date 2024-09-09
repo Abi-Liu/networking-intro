8796e02c3e22671c4e938d7df0159acb7b07b25f
Abi Liu <abiliu018@gmail.com>
2024-09-09 08:51:58 -0700

    modified server

--- echo_serv/server.c
+++ echo_serv/server.c
@@ -1,3 +1,4 @@
+#include <sys/errno.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
@@ -20,5 +21,49 @@ int main(void) {
 
   int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-  bind(sockfd, res->ai_addr, res->ai_addrlen);
+  int bind_status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+  if(bind_status == -1) {
+    int err = errno;
+    printf("%s\n", strerror(errno));
+    return 2;
+  }
+
+  int listen_status = listen(sockfd, 20);
+  if(listen_status == -1) {
+    int err = errno;
+    printf("%s\n", strerror(errno));
+    return 3;
+  }
+
+  struct sockaddr_storage their_addr;
+  int their_addr_size = sizeof their_addr;
+  int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, (unsigned int *)&their_addr_size);
+
+  if(new_fd == -1) {
+    int err = errno;
+    printf("%s\n", strerror(err));
+    return 4;
+  }
+
+
+  char *msg ="hello client!";
+  int sent = send(new_fd, msg, strlen(msg), 0);
+  if(sent == -1) {
+    int err = errno;
+    printf("%s\n", strerror(err));
+    return 5;
+  }
+
+  char rec[100];
+  int received = recv(new_fd, rec, strlen(rec), 0);
+  if(received == -1) {
+    int err = errno;
+    printf("%s\n", strerror(err));
+    return 6;
+  } else if (received == 0) {
+    printf("Client connection has closed\n");
+    return 7;
+  }
+
+  printf("%s\n", rec);
 }
